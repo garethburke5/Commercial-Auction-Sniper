ce71d37f8e01f88519a683adfb3f7ab0968cb0c7
garethburke5 <garethburke5@gmail.com>
2026-09-07 02:26:05 +0100

    Test Pugh mixed-use classification

--- tests/test_pugh_area.py
+++ tests/test_pugh_area.py
@@ -1,5 +1,5 @@
 import unittest
-from collectors.pugh import _floor_area_sqft
+from collectors.pugh import _floor_area_sqft, _pugh_property_type
 
 
 class PughAreaTests(unittest.TestCase):
@@ -14,6 +14,13 @@ class PughAreaTests(unittest.TestCase):
     def test_uses_largest_plausible_area_when_no_total_label_exists(self):
         text = "Front office 352 sq ft. Rear office 744 sq ft. Building extends to 1,607 sq ft."
         self.assertEqual(_floor_area_sqft(text), 1607.0)
+
+    def test_commercial_units_and_apartments_are_mixed_use(self):
+        text = "Mixed use investment comprising two apartments and two commercial units with overall NIA 1,975 sq ft."
+        self.assertEqual(_pugh_property_type(text), "Mixed Use")
+
+    def test_retail_only_remains_retail(self):
+        self.assertEqual(_pugh_property_type("Freehold retail premises comprising two retail units"), "Retail")
 
 
 if __name__ == "__main__":
