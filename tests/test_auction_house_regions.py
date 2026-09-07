817a753977a90a1e6b3704d5d0ba7347f42905bb
garethburke5 <garethburke5@gmail.com>
2026-09-07 07:40:03 +0100

    Test rich Auction House UUID detail recovery

--- tests/test_auction_house_regions.py
+++ tests/test_auction_house_regions.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 
 from collectors.auction_house_regions import (
     _local_card, _commercialish, _prior_or_withdrawn,
-    _card_address, _fallback_catalogue_lot,
+    _card_address, _fallback_catalogue_lot, _direct_first_party_lot,
 )
 
 
@@ -51,6 +51,44 @@ class AuctionHouseRegionTests(unittest.TestCase):
         self.assertEqual(lot.guide_price, 120000)
         self.assertEqual(lot.property_type, "Mixed Use")
         self.assertEqual(lot.auction_date, "2026-09-09")
+
+    def test_uuid_detail_recovery_keeps_rich_first_party_particulars(self):
+        html='''<html><head><meta property="og:image" content="https://wales.auctionhouse.co.uk/images/lot-cardigan.jpg"></head><body>
+          <main>
+            <h1>Lot 25: 7-8 High Street, Cardigan, SA43 1HJ</h1>
+            <p>Retail Investment. Guide Price £275,000. Freehold.</p>
+            <p>Ground floor retail unit extending to approximately 2,336 sq ft with four flats above sold off.</p>
+            <p>Let to The Works Stores Limited on a five year lease from 10 December 2025 at £33,000 per annum.</p>
+            <p>Prominent town centre position.</p>
+          </main>
+        </body></html>'''
+        def fetcher(_url): return BeautifulSoup(html,'lxml')
+        lot=_direct_first_party_lot(
+            "Auction House Wales",
+            "https://wales.auctionhouse.co.uk/lot/details/11111111-1111-1111-1111-111111111111",
+            "Lot 25 *Guide | £275,000 Retail Investment 7-8 High Street, Cardigan, SA43 1HJ",
+            "7-8 High Street, Cardigan, SA43 1HJ",
+            "Lot 25","2026-09-09",fetcher=fetcher,
+        )
+        self.assertIsNotNone(lot)
+        self.assertEqual(lot.address,"7-8 High Street, Cardigan, SA43 1HJ")
+        self.assertEqual(lot.guide_price,275000)
+        self.assertEqual(lot.annual_rent,33000)
+        self.assertEqual(lot.area_sqft,2336)
+        self.assertEqual(lot.tenure,"Freehold")
+        self.assertIsNotNone(lot.image_url)
+
+    def test_uuid_detail_recovery_extracts_mixed_use_and_listed_status(self):
+        html='''<main><h1>20 The Struet, Brecon, LD3 7LL</h1>
+        <p>Mixed Use. Guide £99,000. Freehold Grade II Listed property.</p>
+        <p>Vacant ground floor commercial accommodation with a two bedroom apartment above. Development potential STP.</p></main>'''
+        lot=_direct_first_party_lot(
+            "Auction House Wales","https://wales.auctionhouse.co.uk/lot/details/22222222-2222-2222-2222-222222222222",
+            "Lot 45 Mixed Use Guide £99,000","20 The Struet, Brecon, LD3 7LL","Lot 45","2026-09-09",
+            fetcher=lambda _url: BeautifulSoup(html,'lxml'))
+        self.assertEqual(lot.property_type,"Mixed Use")
+        self.assertEqual(lot.listed_status,"Grade II Listed")
+        self.assertTrue(lot.development_potential)
 
 
 if __name__ == "__main__":
